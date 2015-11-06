88aef5777f6c2571d6f16a5eee67afe91538fab2
sheng zhang <shengzhang@shengs-MacBook-Air.local>
2015-11-06 13:13:15 -0600

    Initial Commit

---
+++ filter2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  filter2
+//
+//  Created by sheng zhang on 11/6/15.
+//  Copyright (c) 2015 Sheng Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
